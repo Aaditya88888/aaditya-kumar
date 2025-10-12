9fd2d0a358b8bc028cfc59d79fedaef8462560bc
Aaditya Kumar <kumaraaditya523@gmail.com>
2025-10-12 17:15:08 +0100

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,311 @@
+// Skills Section Logo's
+import htmlLogo from "./assets/tech_logo/html.png";
+import cssLogo from "./assets/tech_logo/css.png";
+import sassLogo from "./assets/tech_logo/sass.png";
+import javascriptLogo from "./assets/tech_logo/javascript.png";
+import reactjsLogo from "./assets/tech_logo/reactjs.png";
+import angularLogo from "./assets/tech_logo/angular.png";
+import reduxLogo from "./assets/tech_logo/redux.png";
+import nextjsLogo from "./assets/tech_logo/nextjs.png";
+import tailwindcssLogo from "./assets/tech_logo/tailwindcss.png";
+import gsapLogo from "./assets/tech_logo/gsap.png";
+import materialuiLogo from "./assets/tech_logo/materialui.png";
+import bootstrapLogo from "./assets/tech_logo/bootstrap.png";
+import springbootLogo from "./assets/tech_logo/springboot.png";
+import nodejsLogo from "./assets/tech_logo/nodejs.png";
+import expressjsLogo from "./assets/tech_logo/express.png";
+import mysqlLogo from "./assets/tech_logo/mysql.png";
+import mongodbLogo from "./assets/tech_logo/mongodb.png";
+import firebaseLogo from "./assets/tech_logo/firebase.png";
+import cLogo from "./assets/tech_logo/c.png";
+import cppLogo from "./assets/tech_logo/cpp.png";
+import javaLogo from "./assets/tech_logo/java.png";
+import pythonLogo from "./assets/tech_logo/python.png";
+import typescriptLogo from "./assets/tech_logo/typescript.png";
+import gitLogo from "./assets/tech_logo/git.png";
+import githubLogo from "./assets/tech_logo/github.png";
+import vscodeLogo from "./assets/tech_logo/vscode.png";
+import postmanLogo from "./assets/tech_logo/postman.png";
+import mcLogo from "./assets/tech_logo/mc.png";
+import figmaLogo from "./assets/tech_logo/figma.png";
+import netlifyLogo from "./assets/tech_logo/netlify.png";
+import vercelLogo from "./assets/tech_logo/vercel.png";
+import postgreLogo from "./assets/tech_logo/postgre.png";
+import csharpLogo from "./assets/tech_logo/csharp.png";
+
+// Experience Section Logo's
+import webverseLogo from "./assets/company_logo/webverse_logo.png";
+import agcLogo from "./assets/company_logo/agc_logo.png";
+import newtonschoolLogo from "./assets/company_logo/newtonschool_logo.png";
+
+// Education Section Logo's
+import glaLogo from "./assets/education_logo/gla_logo.png";
+import bsaLogo from "./assets/education_logo/bsa_logo.png";
+import vpsLogo from "./assets/education_logo/vps_logo.png";
+
+// Project Section Logo's
+import githubdetLogo from "./assets/work_logo/github_det.png";
+import csprepLogo from "./assets/work_logo/cs_prep.png";
+import movierecLogo from "./assets/work_logo/movie_rec.png";
+import taskremLogo from "./assets/work_logo/task_rem.png";
+import npmLogo from "./assets/work_logo/npm.png";
+import webverLogo from "./assets/work_logo/web_dig.png";
+import cmLogo from "./assets/work_logo/cm.png";
+import imagesearchLogo from "./assets/work_logo/image_search.png";
+import removebgLogo from "./assets/work_logo/remove_bg.png";
+
+import codeReview from "./assets/code-review.jpg";
+import componentGenerator from "./assets/component-generator-image.png";
+import dictionaryApp from "./assets/Dictionary-image.png";
+import geminiChat from "./assets/Gemini-chat.png";
+import musicImage from "./assets/music-image.jpeg";
+import mysteryMessages from "./assets/mystery-message.png";
+
+export const SkillsInfo = [
+  {
+    title: "Frontend",
+    skills: [
+      { name: "HTML", logo: htmlLogo },
+      { name: "CSS", logo: cssLogo },
+      // { name: 'SASS', logo: sassLogo },
+      // { name: "JavaScript", logo: javascriptLogo },
+      { name: "React JS", logo: reactjsLogo },
+      // { name: "Angular", logo: angularLogo },
+      { name: "Redux", logo: reduxLogo },
+      { name: "Next JS", logo: nextjsLogo },
+      { name: "Tailwind CSS", logo: tailwindcssLogo },
+      // { name: "GSAP", logo: gsapLogo },
+      // { name: "Material UI", logo: materialuiLogo },
+      { name: "Bootstrap", logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      // { name: "Springboot", logo: springbootLogo },
+      { name: "Node JS", logo: nodejsLogo },
+      { name: "Express JS", logo: expressjsLogo },
+      // { name: "MySQL", logo: mysqlLogo },
+      { name: "MongoDB", logo: mongodbLogo },
+      { name: "Firebase", logo: firebaseLogo },
+      { name: "PostgreSQL", logo: postgreLogo },
+    ],
+  },
+  {
+    title: "Languages",
+    skills: [
+      { name: "C", logo: cLogo },
+      // { name: "C++", logo: cppLogo },
+      { name: "Java", logo: javaLogo },
+      // { name: "C-Sharp", logo: csharpLogo },
+      { name: "JavaScript", logo: javascriptLogo },
+      { name: "TypeScript", logo: typescriptLogo },
+      { name: "Python", logo: pythonLogo },
+    ],
+  },
+  {
+    title: "Tools",
+    skills: [
+      { name: "Git", logo: gitLogo },
+      { name: "GitHub", logo: githubLogo },
+      { name: "VS Code", logo: vscodeLogo },
+      { name: "Postman", logo: postmanLogo },
+      { name: "Compass", logo: mcLogo },
+      { name: "Vercel", logo: vercelLogo },
+      { name: "Netlify", logo: netlifyLogo },
+      // { name: "Figma", logo: figmaLogo },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: webverseLogo,
+    role: "Fullstack Developer",
+    company: "HeyFynix",
+    date: "September 2025 - Present",
+    desc: "I am currently working as a Full Stack Developer Intern at HeyFynix, contributing to frontend and backend development using the MERN stack, Next.js, and TypeScript. I build interactive user interfaces with React.js and create efficient APIs with Node.js and Express.js, delivering scalable web applications.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+      "TypeScript",
+      "Node JS",
+      "Express JS",
+      "Tailwind CSS",
+      "MongoDb",
+      "Redux",
+      "Context API",
+      "React Router",
+      " Next Js",
+    ],
+  },
+  // {
+  //   id: 1,
+  //   img: agcLogo,
+  //   role: "Fullstack Engineer",
+  //   company: "Agumentik Group of Companies",
+  //   date: "July 2023 - March 2024",
+  //   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+  //   skills: [
+  //     "ReactJS",
+  //     "Redux",
+  //     "JavaScript",
+  //     "Tailwind CSS",
+  //     "HTML",
+  //     "CSS",
+  //     "SQL",
+  //   ],
+  // },
+  // {
+  //   id: 2,
+  //   img: newtonschoolLogo,
+  //   role: "Frontend Intern",
+  //   company: "Newton School",
+  //   date: "September 2021 - August 2022",
+  //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+  //   skills: ["HTML", "CSS", "Javascript", "Bootstrap", "Figma", "Material UI"],
+  // },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: bsaLogo,
+    school: "R.D. Engineering College, Ghaziabad",
+    date: "August 2022 - Present",
+    grade: "75%",
+    desc: "Pursuing a B.Tech in Computer Science with a specialization in Internet of Things (IoT). Gained hands-on experience in web development, backend development, and IoT projects while building practical skills in modern technologies like React.js, Node.js, and MongoDB.",
+    degree: "Bachelor of Technology - B.tech (Computer Science - IOT)",
+  },
+  {
+    id: 1,
+    img: vpsLogo,
+    school: "Shri Hans Inter College, Uttar Pradesh",
+    date: "2021-2022",
+    grade: "75%",
+    desc: "Completed class 12 under the UP Board, studying Physics, Chemistry, and Mathematics (PCM).",
+    degree: "UP(XII) - PCM",
+  },
+  {
+    id: 2,
+    img: vpsLogo,
+    school: "Shri Hans Inter College, Uttar Pradesh",
+    date: "2019-2020",
+    grade: "78%",
+    desc: "Completed class 10 under the UP Board, focusing on the Science stream.",
+    degree: "UP(X) - Science Stream",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Component Generator",
+    description:
+      "A MERN-based web application that generates custom components based on user input. Integrated with Gemini AI to dynamically create tailored components quickly and efficiently.",
+    image: componentGenerator,
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+      "Node JS",
+      "Express JS",
+      "MongoDB",
+      "Postman",
+      "Gemini AI",
+    ],
+    github: "https://github.com/Aaditya88888/Component-Generator",
+    webapp: "https://component-generator-frontend.onrender.com",
+  },
+  {
+    id: 1,
+    title: "Mystery Messages",
+    description:
+      "A Next.js application that allows users to send messages anonymously. It uses NextAuth for secure authentication and Zod for input validation. React Hook Form and Shadcn UI make the interface user-friendly and responsive, while MongoDB ensures reliable message storage. Resend is integrated for seamless message delivery.",
+    image: mysteryMessages,
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+      "Next.js",
+      "NextAuth",
+      "Zod",
+      "Shadcn UI",
+      "React Hook Form",
+      "MongoDB",
+      "Resend",
+      "Postman",
+    ],
+    github: "https://github.com/Aaditya88888/mystery-messages",
+    webapp: "https://mystery-messages-lake.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Gemini Chat",
+    description:
+      "A real-time chat application built with React JS and Tailwind CSS that allows users to interact with Gemini AI, similar to ChatGPT. Users can generate instant responses, making conversations smooth and dynamic. Integrated Gemini AI ensures accurate and context-aware replies in real time.",
+    image: geminiChat,
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+      "Tailwind CSS",
+      "Gemini AI",
+    ],
+    github: "https://github.com/Aaditya88888/Gemini-Chat",
+    webapp: "https://gemini-chat-kohl.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Music School",
+    description:
+      "A modern web application built with Next.js and React.js to manage and showcase music courses. The interface is designed using Acertenity UI, providing a responsive and user-friendly experience for students and instructors. This project emphasizes clean design and smooth navigation for an engaging learning platform.",
+    image: musicImage,
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Next.js",
+      "React JS",
+      "Tailwind CSS",
+      "Aceternity UI",
+    ],
+    github: "https://github.com/Aaditya88888/music-school",
+    webapp: "https://music-school-three-sigma.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Code Reviewer",
+    description:
+      "A web application where users can write code and receive detailed feedback, including mistakes, improvements, and better approaches. Built with React JS and Express.js, it integrates Gemini AI to provide intelligent, context-aware code analysis.",
+    image: codeReview,
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+      "Express JS",
+      "Tailwind CSS",
+      "Gemini AI",
+      "Postman",
+    ],
+    github: "https://github.com/Aaditya88888/Code-Review",
+    webapp: "https://code-reviewer-frontend-70xs.onrender.com",
+  },
+  {
+    id: 5,
+    title: "Dictionary App",
+    description:
+      "A React JS application that allows users to find meanings, examples, and synonyms of any word. The app includes a pronunciation feature so users can listen to words. Built with Tailwind CSS for a clean and responsive user interface, it provides a smooth and interactive learning experience.",
+    image: dictionaryApp,
+    tags: ["HTML", "CSS", "JavaScript", "React JS", "Tailwind CSS"],
+    github: "https://github.com/Aaditya88888/Dictionary-App",
+    webapp: "https://dictionary-app-rho-lemon.vercel.app/",
+  },
+];
